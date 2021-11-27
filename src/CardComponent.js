72f15f7f1446c8ea055dc34b4e42e3c97d5394ea
Slim <sazwerling@gmail.com>
2021-11-27 13:54:15 -0600

    rotate on click

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -4,12 +4,23 @@ import React from 'react';
 class Card extends React.Component{
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {showBack: false}
+    }
+
+    handleClick = () => {
+        this.setState({showBack: !this.state.showBack})
+        console.log(this.state)
     }
 
     render(){
+        
         return(
-            <div className="card">LENGUA</div>
+            <div className="scene" onClick={this.handleClick}>
+            <div className={"card " + (this.state.showBack ? "is-flipped" : "")}>
+              <div className="card__face card__face--front">this is the front</div>
+              <div className="card__face card__face--back">this is on the back and more stuff</div>
+            </div>
+          </div>
         )
     }
 }
