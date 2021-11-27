ea0916ebdb4d781aea5872f99de26eafcbf9529d
Slim <sazwerling@gmail.com>
2021-11-26 22:36:06 -0600

    first commit

---
+++ src/CardComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Card extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div className="card">LENGUA</div>
+        )
+    }
+}
+
+export default Card;
