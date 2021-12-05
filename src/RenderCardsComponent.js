2e1e6dfe56261a331c22f296af768458d22020e7
Slim <sazwerling@gmail.com>
2021-12-05 09:26:52 -0600

    w button

--- src/RenderCardsComponent.js
+++ src/RenderCardsComponent.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function RenderCards({ words }) {
    const cards = words.cards.map((card) => (
-      <div key={card.Id} className="col-4 scene" onClick={() => console.log('clicked')}>
+      <div key={card.Id} className="col-4 scene">
+         
          <div className="card">
+         
             <div className="card-body d-flex justify-content-center align-items-center card__face card__face--front">
+            <button onClick={() => console.log('delete')}>delete</button>
                {card.front}
             </div>
             <div className="card-body d-flex justify-content-center align-items-center card__face card__face--back">
